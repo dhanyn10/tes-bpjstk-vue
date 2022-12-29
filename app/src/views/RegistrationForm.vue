052c67bc7be9c3e2b93d21e140b8f2d1278e733e
dhanyn10 <dhanyn.dhan@gmail.com>
2022-12-29 23:52:37 +0700

    send + reset

--- app/src/views/RegistrationForm.vue
+++ app/src/views/RegistrationForm.vue
@@ -5,7 +5,7 @@
         <b-col md="4" offset-md="4">
           <b-card header="Register Form">
             <b-card-text>
-              <b-form>
+              <b-form @submit="kirim">
                 <b-form-group>
                   <b-form-input type="text" placeholder="nama" v-model="nama" required></b-form-input>
                 </b-form-group>
@@ -29,7 +29,8 @@
                   <b-form-input type="password" v-model="re_password" placeholder="Retype Password" required></b-form-input>            
                 </b-form-group>
                 <b-form-group>
-                  <b-button type="submit" variant="primary" @click="kirim">Register</b-button>
+                  <b-button type="submit" variant="primary">Register</b-button>
+                  <b-button type="button" class="float-right" @click="reset" variant="outline-secondary">Reset</b-button>
                 </b-form-group>
               </b-form>
             </b-card-text>
@@ -44,6 +45,7 @@
 import Vue from 'vue';
 import { BContainer, FormPlugin } from 'bootstrap-vue';
 import moment from 'moment'
+import axios from 'axios'
 
 Vue.component('b-container', BContainer)
 Vue.use(FormPlugin)
@@ -56,27 +58,32 @@ export default {
       jk_selected: null,
       jk_options: [
         {value: null,text: "pilih jenis kelamin"},
-        {value: 1,text: "Laki-laki"},
-        {value: 0,text: "Perempuan"}
+        {value: "L",text: "Laki-laki"},
+        {value: "P",text: "Perempuan"}
       ],
       tanggalLahir: '',
-      freshgrad: 0,
+      freshgrad: null,
       username: '',
       password: '',
       re_password: ''
     }
   },
   methods: {
-    kirim () {
+    kirim (e) {
+      e.preventDefault()
+      let sendForm = true
       // validasi nama
       if(this.nama.length < 20) {
         alert('nama terlalu pendek')
+        sendForm = false
       } else if(this.nama.length > 30) {
         alert('nama terlalu panjang')
+        sendForm = false
       }
       // validasi jenis kelamin
       if(this.jk_selected == null) {
         alert('pilih jenis kelamin')
+        sendForm = false
       }
       // validasi umur
       let dateAwal = moment().format('L');
@@ -86,7 +93,41 @@ export default {
       let selisihTahun = dateAwal.diff(dateAkhir, 'years')
       if(selisihTahun < 20) {
         alert('dibawah umur')
+        sendForm = false
       }
+      if(this.password !==  this.re_password) {
+        alert('password salah')
+        sendForm = false
+      }
+      if(sendForm) {
+        axios.post("http://localhost:8000/register", {
+            nama: this.nama,
+            jk: this.jk_selected,
+            tglLahir: this.tanggalLahir,
+            freshGrad: this.freshgrad,
+            username: this.username,
+            password: this.password
+          }
+        ).then(response => {
+          let data = response.data
+          let message = data.message
+          if(message == "registered")
+            alert("berhasil registrasi")
+          else if(message == "user exist")
+            alert("nama sudah digunakan")
+          else if(message == "username exist")
+            alert("username sudah digunakan")
+        })
+      }
+    },
+    reset () {
+      this.nama = ""
+      this.jk_selected = null
+      this.tanggalLahir = ""
+      this.freshgrad = null
+      this.username = null
+      this.password = null
+      this.re_password = null
     }
   }
 }
