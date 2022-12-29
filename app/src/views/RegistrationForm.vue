533724ad9e0cb23319de449c28bfdc2db7bf9b7b
dhanyn10 <dhanyn.dhan@gmail.com>
2022-12-29 22:14:33 +0700

    validasi

--- app/src/views/RegistrationForm.vue
+++ app/src/views/RegistrationForm.vue
@@ -5,61 +5,31 @@
         <b-col md="4" offset-md="4">
           <b-card header="Register Form">
             <b-card-text>
-              <b-form method="post">
+              <b-form>
                 <b-form-group>
-                  <b-form-input
-                    type="text"
-                    placeholder="nama"
-                    v-model="nama"
-                    required
-                  ></b-form-input>
+                  <b-form-input type="text" placeholder="nama" v-model="nama" required></b-form-input>
                 </b-form-group>
                 <b-form-group>
-                  <b-form-input
-                    type="text"
-                    v-model="jk"
-                    placeholder="jenis kelamin"
-                    required
-                  ></b-form-input>
+                  <b-form-select v-model="jk_selected" :options="jk_options" required></b-form-select>
                 </b-form-group>
                 <b-form-group>
-                  <b-form-input
-                    type="date"
-                    v-model="tanggalLahir"
-                    placeholder="Tanggal Lahir"
-                    required
-                  ></b-form-input>
+                  <b-form-input type="date" v-model="tanggalLahir" placeholder="Tanggal Lahir" required></b-form-input>
                 </b-form-group>
                 <b-form-group label="Freshgraduated?">
-                  <b-form-radio v-model="freshgrad" name="some-radios" value="A">Yes</b-form-radio>
-                  <b-form-radio v-model="freshgrad" name="some-radios" value="B">No</b-form-radio>
+                  <b-form-radio v-model="freshgrad" value="1">Yes</b-form-radio>
+                  <b-form-radio v-model="freshgrad" value="0">No</b-form-radio>
                 </b-form-group>
                 <b-form-group>
-                  <b-form-input
-                    type="text"
-                    v-model="username"
-                    placeholder="username"
-                    required
-                  ></b-form-input>            
+                  <b-form-input type="text" v-model="username" placeholder="username" required></b-form-input>            
                 </b-form-group>
                 <b-form-group>
-                  <b-form-input
-                    type="password"
-                    v-model="password"
-                    placeholder="password"
-                    required
-                  ></b-form-input>            
+                  <b-form-input type="password" v-model="password" placeholder="password" required></b-form-input>            
                 </b-form-group>
                 <b-form-group>
-                  <b-form-input
-                    type="password"
-                    v-model="re_password"
-                    placeholder="Retype Password"
-                    required
-                  ></b-form-input>            
+                  <b-form-input type="password" v-model="re_password" placeholder="Retype Password" required></b-form-input>            
                 </b-form-group>
                 <b-form-group>
-                  <b-button variant="primary" @click="kirim">Register</b-button>
+                  <b-button type="submit" variant="primary" @click="kirim">Register</b-button>
                 </b-form-group>
               </b-form>
             </b-card-text>
@@ -83,7 +53,12 @@ export default {
   data() {
     return {
       nama: '',
-      jk: '',
+      jk_selected: null,
+      jk_options: [
+        {value: null,text: "pilih jenis kelamin"},
+        {value: 1,text: "Laki-laki"},
+        {value: 0,text: "Perempuan"}
+      ],
       tanggalLahir: '',
       freshgrad: 0,
       username: '',
@@ -93,14 +68,23 @@ export default {
   },
   methods: {
     kirim () {
+      // validasi nama
+      if(this.nama.length < 20) {
+        alert('nama terlalu pendek')
+      } else if(this.nama.length > 30) {
+        alert('nama terlalu panjang')
+      }
+      // validasi jenis kelamin
+      if(this.jk_selected == null) {
+        alert('pilih jenis kelamin')
+      }
       // validasi umur
       let dateAwal = moment().format('L');
       let dateAkhir = moment(this.tanggalLahir).format('L')
       dateAwal = moment(dateAwal)
       dateAkhir = moment(dateAkhir)
       let selisihTahun = dateAwal.diff(dateAkhir, 'years')
-      if(selisihTahun < 20)
-      {
+      if(selisihTahun < 20) {
         alert('dibawah umur')
       }
     }
