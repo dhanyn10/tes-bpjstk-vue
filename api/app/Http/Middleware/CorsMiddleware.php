bafe7a95ff8253050c2c7aa63631b9d1c5388f0d
dhanyn10 <dhanyn.dhan@gmail.com>
2022-12-30 00:58:57 +0700

    fix cors

--- api/app/Http/Middleware/CorsMiddleware.php
+++ api/app/Http/Middleware/CorsMiddleware.php
@@ -24,6 +24,12 @@ class CorsMiddleware
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
+
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
         $response = $next($request);
         foreach($headers as $key => $value)
         {
